4a59595fe07127ac8243a597e3df2fbeb3aff7c5
Duvan Lopez <stevenbarcalop@gmail.com>
2018-07-26 11:14:38 -0500

    commit 3

--- src/medidas/Medidas.java
+++ src/medidas/Medidas.java
@@ -12,42 +12,12 @@ import java.util.Scanner;
  * @author Wilson
  */
 public class Medidas {
-    private static int e;
-
-    /**
+        /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
-        float medidas[]=new float[50];
-        float CanVara[]= new float[10];
-        int cantidad;
-        int tira;
-        float sumatoria = 0;
-        float maxvara=585;
-        
-        Scanner sc=new Scanner(System.in);
-        
-        System.out.println("Cantidad de medidas que va a introducir max 50"); 
-       cantidad= sc.nextInt();
-       
-       for(int i=0;i<=(cantidad-1);i++)
-       {
-            System.out.println("Introdusca medida numero " +(i+1) );
-            medidas[i]=sc.nextFloat();
-            sumatoria=medidas[i]+sumatoria;
-            if(sumatoria>=maxvara)
-            {
-                int e=0;
-                sumatoria=sumatoria-medidas[i];
-                CanVara[e]=sumatoria;
-                e++;
-                sumatoria=0;
-            }
-            
-       }
-        System.out.println(CanVara[0]);
-        System.out.println(sumatoria);
+        System.out.println("Hello world");
        
         
         
